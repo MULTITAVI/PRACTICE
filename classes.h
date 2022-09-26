3c512bd6e0fe6fc79218ef5b8ff146369080e50d
MULTITAVI <94232251+MULTITAVI@users.noreply.github.com>
2022-09-26 23:47:56 +0300

    Add files via upload

---
+++ classes.h
@@ -0,0 +1,291 @@
+/*
+* Данный файл содержит инструменты взаимодействия с данными пользователя
+*/
+
+#ifndef _classes_h
+#define _classes_h
+#include <string>
+#include <ctime>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+const unsigned FULL_NAME_CATEGORY = 1;
+const unsigned DATE_OF_BIRTH_CATEGORY = 2;
+const unsigned COMPOUND_CATEGORY = 3;
+const unsigned IS_IN_THE_TEAM_CATEGORY = 4;
+const unsigned PARTICIPATION_IN_THE_LAST_MATCH_CATEGORY = 5;
+const unsigned NUMBER_OF_YELLOW_CARDS_CATEGORY = 6;
+const unsigned MAX_LENGTH_OF_DATE = 20;
+const std::string CORE_TEAM = "Core member";
+const std::string PARTICIPATE_IN_THE_LAST_MATCH = "Participated in the last match";
+const unsigned NUMBER_OF_YELLOW_CARDS = 1;
+
+class Player {
+public:
+	// Конструктор, не принимающий аргументов, создающий экземпляр класса со стандартными значениями
+	// Имя игрока = 'Example', Дата рождения - 1 января 1900 года, Состав - дополнительный, 
+	// Состоит в сборной - нет, Участие в последнем матче - нет, Кол-во желтых карточек - 0
+	Player() { 
+		playersName_ = "Example";
+		dateOfBirth_.tm_year = 1900;
+		dateOfBirth_.tm_mon = 0;
+		dateOfBirth_.tm_mday = 1;
+		compound_ = false;
+		isInTheTeam_ = false;
+		participationInTheLastMatch_ = false;
+		numberOfYellowCards_ = 0;
+	}
+
+	// Конструктор, создающий экземпляр класса со значениями, переданными пользователем
+	Player(std::string playersName, std::tm dateOfBirth, bool compound, bool isInTheTeam, bool participationInTheLastMatch,
+		unsigned numberOfYellowCards) : playersName_(playersName), dateOfBirth_(dateOfBirth), compound_(compound),
+		isInTheTeam_(isInTheTeam), participationInTheLastMatch_(participationInTheLastMatch), numberOfYellowCards_(numberOfYellowCards) {}
+
+	// Конструктор копирования, служащий для создания объекта путем копирования элементов другого объекта
+	Player(const Player& other) { 
+		playersName_ = other.playersName_;
+		dateOfBirth_ = other.dateOfBirth_;
+		compound_ = other.compound_;
+		isInTheTeam_ = other.isInTheTeam_;
+		participationInTheLastMatch_ = other.participationInTheLastMatch_;
+		numberOfYellowCards_ = other.numberOfYellowCards_;
+	}
+
+	// Деструктор, вызывается при уничтожении объекта
+	~Player() {}
+
+	// Перегрузка оператора <<, для шаблонизирования представления информации о полях класса
+	friend std::ostream& operator<<(std::ostream& os, Player& other);
+
+	// Перегрузка оператора >>, для универсального ввода информации, необходимой при создании класса
+	friend std::istream& operator>>(std::istream& is, Player& other);
+
+	// Метод для получения ФИО игрока
+	std::string getPlayersName() {
+		return playersName_;
+	}
+
+	// Метод для получения даты рождения игрока
+	std::string getDateOfBirth() {
+		char str[MAX_LENGTH_OF_DATE];
+
+		if (std::strftime(str, sizeof(str), "%D", &dateOfBirth_)) {
+			return str;
+		}
+		else {
+			return "Unable to get date of birth";
+		}
+	}
+
+	// Метод для получения информации о том, в основном составе или на скамейке запасных находится игрок
+	std::string getCompound() {
+		if (compound_) {
+			return "Core member";
+		}
+		else {
+			return "Bench member";
+		}
+	}
+
+	// Метод для получения информации о том, состоит ли игрок в сборной или нет
+	std::string getIsInTheTeam() {
+		if (isInTheTeam_) {
+			return "Member of the national team";
+		}
+		else {
+			return "Not a member of the national team";
+		}
+	}
+
+	// Метод для получения информации о том, участвовал ли игрок в последнем матче
+	std::string getParticipationInTheLastMatch() {
+		if (participationInTheLastMatch_) {
+			return "Participated in the last match";
+		}
+		else {
+			return "Didn't participated in the last match";
+		}
+	}
+
+	// Метод для получения информации о кол-во желтых карточек игрока
+	unsigned getNumberOfYellowCards() {
+		return numberOfYellowCards_;
+	}
+
+	// Перегрузка оператора присваивания для возможности присваивания экземпляра класса
+	Player& operator=(const Player& other) {
+		if (this != &other) {
+			playersName_ = other.playersName_;
+			dateOfBirth_ = other.dateOfBirth_;
+			compound_ = other.compound_;
+			isInTheTeam_ = other.isInTheTeam_;
+			participationInTheLastMatch_ = other.participationInTheLastMatch_;
+			numberOfYellowCards_ = other.numberOfYellowCards_;
+		}
+		return *this;
+	}
+
+	// Перегрузка оператора равенства, для сравнения двух элементов типа Player
+	bool operator==(const Player& other) {
+		if (playersName_ == other.playersName_ && dateOfBirth_.tm_year == other.dateOfBirth_.tm_year &&
+			dateOfBirth_.tm_mon == other.dateOfBirth_.tm_mon && dateOfBirth_.tm_mday == other.dateOfBirth_.tm_mday &&
+			compound_ == other.compound_ && isInTheTeam_ == other.isInTheTeam_ &&
+			participationInTheLastMatch_ == other.participationInTheLastMatch_ && numberOfYellowCards_ == other.numberOfYellowCards_) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+private:
+	std::string playersName_;
+	std::tm dateOfBirth_{};
+	bool compound_{};
+	bool isInTheTeam_{};
+	bool participationInTheLastMatch_{};
+	unsigned numberOfYellowCards_{};
+};
+
+class PlayersList {
+public:
+	// Конструктор, создающий пустой контейнер для хранения объектов типа Player
+	PlayersList() {}
+
+	// Конструктор, создающий заполненный контейнер, на основе переданного
+	PlayersList(std::vector<Player> players) {
+		playersList_ = players;
+	}
+
+	// Деструктор, вызывается при уничтожении объекта
+	~PlayersList() {}
+
+	// Метод, служащий для добавления элементов, в конец контейнера
+	// На вход принимает экземпляр типа Player, который необходимо добавить
+	void add(const Player& player) {
+		playersList_.push_back(player);
+	}
+
+	// Метод, служащий для удаления элемента, выбранного пользователем
+	// На вход принимает номер элемента из списка, который необходимо удалить
+	void remove(unsigned number) {
+		if ((int)number <= (playersList_.end() - playersList_.begin())) {
+			playersList_.erase(playersList_.begin() + number - 1);
+		}
+	}
+
+	// Метод, служащий для сортировки одного из параметров класса Player, в зависимости от переданной функции сравнения
+	// На вход принимает функцию сравнения для выбранного параметра
+	void sort(bool (*cmp)(Player left, Player right)) {
+		std::sort(playersList_.begin(), playersList_.end(), cmp);
+	}
+
+	// Метод, служащий для поиска переданного элемента, в выбранной категории.
+	// На вход принимает параметр и значение, которое нужно найти
+	// Возвращает список всех элементов, содержащих данное значение в выбранной категории
+	// Категории: 1 - ФИО игрока, 2 - дата рождения, 3 - состав, 4 - состоит в сборной,
+	// 5 - участие в последнем матче, 6 - кол-во желтых карточек
+	std::vector<Player> find(unsigned parameter) {
+		std::vector<Player> found;
+		
+		try
+		{
+			switch (parameter)
+			{
+			case FULL_NAME_CATEGORY:
+			{
+				std::string value;
+				std::string blankShot;
+				std::getline(std::cin, blankShot);
+				std::getline(std::cin, value);
+				std::for_each(playersList_.begin(), playersList_.end(), [&value, &found](Player& element) { if (element.getPlayersName() == value) { found.push_back(element); } });
+				break;
+			}
+			case DATE_OF_BIRTH_CATEGORY:
+			{
+				std::string value;
+				std::string blankShot;
+				std::getline(std::cin, blankShot);
+				std::getline(std::cin, value);
+				std::for_each(playersList_.begin(), playersList_.end(), [&value, &found](Player& element) { if (element.getDateOfBirth() == value) { found.push_back(element); } });
+				break;
+			}
+			case COMPOUND_CATEGORY:
+			{
+				std::string value;
+				std::string blankShot;
+				std::getline(std::cin, blankShot);
+				std::getline(std::cin, value);
+				std::for_each(playersList_.begin(), playersList_.end(), [&found, &value](Player& element) { if (element.getCompound() == value) { found.push_back(element); } });
+				break;
+			}
+			case IS_IN_THE_TEAM_CATEGORY:
+			{
+				std::string value;
+				std::string blankShot;
+				std::getline(std::cin, blankShot);
+				std::getline(std::cin, value);
+				std::for_each(playersList_.begin(), playersList_.end(), [&value, &found](Player& element) { if (element.getIsInTheTeam() == value) { found.push_back(element); } });
+				break;
+			}
+			case PARTICIPATION_IN_THE_LAST_MATCH_CATEGORY:
+			{
+				std::string value;
+				std::string blankShot;
+				std::getline(std::cin, blankShot);
+				std::getline(std::cin, value);
+				std::for_each(playersList_.begin(), playersList_.end(), [&value, &found](Player& element) { if (element.getParticipationInTheLastMatch() == value) { found.push_back(element); } });
+				break;
+			}
+			case NUMBER_OF_YELLOW_CARDS_CATEGORY:
+			{
+				unsigned value;
+				std::cin >> value;
+				std::for_each(playersList_.begin(), playersList_.end(), [&value, &found](Player& element) { if (element.getNumberOfYellowCards() == value) { found.push_back(element); } });
+				break;
+			}
+			default:
+				break;
+			}
+
+		}
+		catch (const std::exception&)
+		{
+			std::cout << "An error occurred while entering data" << std::endl;
+		}
+		return found;
+	}	
+
+	// Перегрузка оператора << для универсального вывода информации обо всех значениях списка
+	friend std::ostream& operator<<(std::ostream& os, PlayersList& other);
+	
+	// Метод, позволяющий редактировать выбранного пользователя
+	void edit(unsigned number, const Player& player) {
+		if ((int)number <= playersList_.size()) {
+			playersList_[number - 1] = player;
+		}
+	}
+
+	// Метод, для получения списка, содержащего информацию обо всех игроках
+	std::vector<Player> getPlayersList() {
+		return playersList_;
+	}
+
+	// Метод для получения списка игроков, которые состоят в основном составе, участвовали в последней игре и имеют одну жёлтую карточку
+	std::vector<Player> areInTheMainTeamParticipatedInTheLastGameAndHaveOneYellowCard() {
+		std::vector<Player> found;
+
+		std::for_each(playersList_.begin(), playersList_.end(), [&found](Player& element) { if (element.getCompound() == CORE_TEAM &&
+			element.getParticipationInTheLastMatch() == PARTICIPATE_IN_THE_LAST_MATCH && element.getNumberOfYellowCards() == NUMBER_OF_YELLOW_CARDS) {
+			found.push_back(element);
+		} });
+		return found;
+	}
+
+private:
+	std::vector<Player> playersList_;
+};
+
+#endif // !_classes_h
---
+++ main.cpp
@@ -0,0 +1,474 @@
+#include "classes.h"
+#include <limits>
+
+const unsigned SHOW_ITEMS = 1;
+const unsigned ADD_ITEM = 2;
+const unsigned DELETE_ITEM = 3;
+const unsigned SORTING = 4;
+const unsigned SEARCH = 5;
+const unsigned EDIT_ITEM = 6;
+const unsigned CORE_TEAM_PARTICIPATED_IN_THE_LAST_MATCH_ONE_YELLOW_CARD = 7;
+const unsigned EXIT = 8;
+const unsigned SORTING_BY_NAME = 1;
+const unsigned SORTING_BY_DATE_OF_BIRTH = 2;
+const unsigned SORTING_BY_NUMBER_OF_YELLOW_CARDS = 3;
+const unsigned ASCENDING = 1;
+const unsigned DESCENDING = 2;
+const unsigned YEAR_START_POS = 6;
+const unsigned MONTH_START_POS = 0;
+const unsigned DAY_START_POS = 3;
+const unsigned LENGTH_TO_COMPARE = 2;
+
+// Перегрузка оператора << для класса Player, которая выводит в консоль информацию об игроке табулированную друг от друга по два раза
+std::ostream& operator<<(std::ostream& os, Player& other) {
+	os << other.getPlayersName() << "\t\t" << other.getDateOfBirth() << "\t\t" <<
+		other.getCompound() << "\t\t" << other.getIsInTheTeam() << "\t\t" <<
+		other.getParticipationInTheLastMatch() << "\t\t" << other.getNumberOfYellowCards() << "\n";
+	return os;
+}
+
+// Перегрузка оператора >> для класса Player, которая считывает информацию из консоли и присваивает контейнеру Player полученную информацию
+std::istream& operator>>(std::istream& is, Player& other) {
+	try
+	{
+		std::string blankShot;
+		std::string playersName;
+		std::tm dateOfBirth{};
+		bool compound{};
+		bool isInTheTeam{};
+		bool participationInTheLastMatch{};
+		unsigned numberOfYellowCards;
+
+		unsigned mon;
+		unsigned year;
+		
+
+		std::getline(is, blankShot);
+		std::getline(is, playersName);
+		is.exceptions(std::istream::failbit | std::istream::badbit);
+		try
+		{
+			is >> dateOfBirth.tm_mday >> mon >> year;
+
+			dateOfBirth.tm_mon = mon - 1;
+			dateOfBirth.tm_year = year - 1900;
+
+			is >> compound >> isInTheTeam >> participationInTheLastMatch >> numberOfYellowCards;
+
+			Player player = Player(playersName, dateOfBirth, compound, isInTheTeam, participationInTheLastMatch, numberOfYellowCards);
+			other = player;
+		}
+		catch (std::istream::failure e)
+		{
+			std::cout << "Failed to enter data" << std::endl;
+			is.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+			is.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+
+		return is;
+	}
+	catch (const std::exception&)
+	{
+		std::cout << "An error occurred while entering data" << std::endl;
+		return is;
+	}
+}
+
+//  Перегрузка оператора << для класса PlayersList, которая выводит информацию обо всех игроках в виде таблицы
+std::ostream& operator<<(std::ostream& os, PlayersList& playersList) {
+	std::vector<Player> players = playersList.getPlayersList();
+	unsigned step = 1;
+	std::for_each(players.begin(), players.end(), [&os, &step](Player& element) { os << step++ << "\t\t" << element; });
+	
+	return os;
+}
+
+// Функция для сравнения строк в порядке возрастания
+bool comparePlayersNameAscending(Player left, Player right) {
+	return left.getPlayersName() < right.getPlayersName() ? true : false;
+}
+
+// Фунукция для сравнения строк в порядке убывания
+bool comparePlayersNameDescending(Player left, Player right) {
+	return left.getPlayersName() < right.getPlayersName() ? false : true;
+}
+
+// Функция для сравнения дат рождения в порядке возрастания
+bool compareDateOfBirthAscending(Player left, Player right) {
+	if (left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)) {
+		return true;
+	}
+	else if(left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE)) {
+		return true;
+	}
+	else if (left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(DAY_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(DAY_START_POS, LENGTH_TO_COMPARE)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+// Функция для сравнения дат рождения в порядке убывания
+bool compareDateOfBirthDescending(Player left, Player right) {
+	if (left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)) {
+		return false;
+	}
+	else if (left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE)) {
+		return false;
+	}
+	else if (left.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(YEAR_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE) == right.getDateOfBirth().substr(MONTH_START_POS, LENGTH_TO_COMPARE)
+			&& left.getDateOfBirth().substr(DAY_START_POS, LENGTH_TO_COMPARE) < right.getDateOfBirth().substr(DAY_START_POS, LENGTH_TO_COMPARE)) {
+		return false;
+	}
+	else {
+		return true;
+	}
+}
+
+// Функция для сравнения кол-ва желтых карточек в порядке возрастания
+bool compareNumberOfYellowCardsAscending(Player left, Player right) {
+	return left.getNumberOfYellowCards() < right.getNumberOfYellowCards() ? true : false;
+}
+
+// Функция для сравнения кол-ва желтых карточек в порядке убывания
+bool compareNumberOfYellowCardsDescending(Player left, Player right) {
+	return left.getNumberOfYellowCards() < right.getNumberOfYellowCards() ? false : true;
+}
+
+// Функция, отвечающая за показ приветственного меню
+void welcomeMenu() {
+	std::cout << "Welcome to the player data software" << std::endl;
+}
+
+// Функция, отвечающая за показ базовых действий
+void basicActionsMenu() { 
+	std::cout << "Choose one of the actions:" << std::endl <<
+		"1. Show items" << std::endl << "2. Add item" << std::endl <<
+		"3. Delete item" << std::endl << "4. Sorting" << std::endl <<
+		"5. Search" << std::endl << "6. Edit item" << std::endl <<
+		"7. Find first team players who participated in the last match and have one yellow card" << std::endl <<
+		"8. Exit the program" << std::endl;
+}
+
+// Функция, показывающая памятку о том, как правильно вводить данные
+void informationAboutCorrectDataEntry() {
+	std::cout << "To enter the data correctly, follow the following example:" << std::endl <<
+		"The first line is the player's full name, then three numbers are responsible for the day, month and year of the player's birth" << std::endl <<
+		"Further, the number 1 or 0, where 1 means the player of the main team, 0 means the player on the bench" << std::endl <<
+		"Similarly with the next number, 1 - is a member of the team, 0 - is not a member" << std::endl <<
+		"Then again 1 or 0, 1 - participated in the last match, 0 - did not participate" << std::endl <<
+		"The last number is the number of yellow cards" << std::endl;
+}
+
+// Функция, показывающее меню категорий, доступных для сортировки
+void menuWithCategoriesWhenSorting() {
+	std::cout << "Select the category you want to sort by:" << std::endl <<
+		"1. Sorting by name" << std::endl <<
+		"2. Sorting by date of birth" << std::endl <<
+		"3. Sorting by number of yellow cards" << std::endl;
+}
+
+// Функция, показывающее меню сортировки с выбором возрастания или убывания
+void descendingAscendingMenu() {
+	std::cout << "Select sort type:" << std::endl <<
+		"1. Ascending" << std::endl <<
+		"2. Descending" << std::endl;
+}
+
+// Функция, показывающая шаблонный текст о найденных элементах
+void informationAboutWhatWasFound() {
+	std::cout << "Found the following items:" << std::endl;
+}
+
+// Функция, показывающая меню с категориями для поиска
+void menuWithCategoriesWhenSearching() {
+	std::cout << "Select a category to search:" << std::endl <<
+		"1. Player's name" << std::endl <<
+		"2. Player's date of birth" << std::endl <<
+		"3. Main squad or bench" << std::endl <<
+		"4. Member of the national team or not" << std::endl <<
+		"5. Participated in the last match or not" << std::endl <<
+		"6. Number of yellow cards" << std::endl;
+}
+
+// функция, показывающая информацию о верном вводе данных для поиска
+void informationAboutSearch() {
+	std::cout << "Enter the value you want to find" << std::endl <<
+		"Full name - string" << std::endl <<
+		"Date of birth - format string 'mm/dd/yy'" << std::endl <<
+		"Compound - 'Core member' or 'Bench member'" << std::endl <<
+		"Member of the national team - 'Member of the national team' or 'Not a member of the national team'" << std::endl <<
+		"Participation in the last match - 'Participated in the last match' or 'Didn't participated in the last match'" << std::endl <<
+		"For the number oof yellow cards, enter a number" << std::endl;
+}
+
+int main() {
+	welcomeMenu();
+	
+	unsigned choice;
+	PlayersList playersList = PlayersList();
+
+	while (true) {
+		basicActionsMenu();
+
+		std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+		try
+		{
+			std::cin >> choice;
+		}
+		catch (std::istream::failure e)
+		{
+			std::cout << "Failed to enter data" << std::endl;
+			std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+			std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		}
+
+		switch (choice)
+		{
+		case SHOW_ITEMS:
+		{
+			std::cout << playersList;
+			break;
+		}
+		case ADD_ITEM:
+		{
+			informationAboutCorrectDataEntry();
+			Player player = Player();
+			Player blankShot = Player();
+			std::cin >> player;
+			if (player == blankShot) {
+				break;
+			}
+			playersList.add(player);
+			break;
+		}
+		case DELETE_ITEM:
+		{
+			std::cout << playersList;
+			std::cout << "Enter the	number of the element to be removed:" << std::endl;
+			unsigned numberOfTheElementToBeRemoved;
+			std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+			try
+			{
+				std::cin >> numberOfTheElementToBeRemoved;
+				playersList.remove(numberOfTheElementToBeRemoved);
+			}
+			catch (std::istream::failure e)
+			{
+				std::cout << "Failed to enter data" << std::endl;
+				std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			break;
+		}
+		case SORTING:
+		{
+			menuWithCategoriesWhenSorting();
+			unsigned choosenCategory;
+			unsigned ascendingOrDescending;
+			std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+			try
+			{
+				std::cin >> choosenCategory;
+			}
+			catch (std::istream::failure e)
+			{
+				std::cout << "Failed to enter data" << std::endl;
+				std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			switch (choosenCategory)
+			{
+			case SORTING_BY_NAME:
+			{
+				descendingAscendingMenu();
+				std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+				try
+				{
+					std::cin >> ascendingOrDescending;
+				}
+				catch (std::istream::failure e)
+				{
+					std::cout << "Failed to enter data" << std::endl;
+					std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				}
+				switch (ascendingOrDescending)
+				{
+				case ASCENDING:
+				{
+					playersList.sort(comparePlayersNameAscending);
+					break;
+				}
+				case DESCENDING:
+				{
+					playersList.sort(comparePlayersNameDescending);
+					break;
+				}
+				default:
+				{
+					std::cout << "Incorrect data entered" << std::endl;
+					break;
+				}
+				}
+				break;
+			}
+			case SORTING_BY_DATE_OF_BIRTH:
+			{
+				descendingAscendingMenu();
+				std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+				try
+				{
+					std::cin >> ascendingOrDescending;
+				}
+				catch (std::istream::failure e)
+				{
+					std::cout << "Failed to enter data" << std::endl;
+					std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				}
+				switch (ascendingOrDescending)
+				{
+				case ASCENDING:
+				{
+					playersList.sort(compareDateOfBirthAscending);
+					break;
+				}
+				case DESCENDING:
+				{
+					playersList.sort(compareDateOfBirthDescending);
+					break;
+				}
+				default:
+				{
+					std::cout << "Incorrect data entered" << std::endl;
+					break;
+				}
+				}
+				break;
+			}
+			case SORTING_BY_NUMBER_OF_YELLOW_CARDS:
+			{
+				descendingAscendingMenu();
+				std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+				try
+				{
+					std::cin >> ascendingOrDescending;
+				}
+				catch (std::istream::failure e)
+				{
+					std::cout << "Failed to enter data" << std::endl;
+					std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+					std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+				}
+				switch (ascendingOrDescending)
+				{
+				case ASCENDING:
+				{
+					playersList.sort(compareNumberOfYellowCardsAscending);
+					break;
+				}
+				case DESCENDING:
+				{
+					playersList.sort(compareNumberOfYellowCardsDescending);
+					break;
+				}
+				default:
+				{
+					std::cout << "Incorrect data entered" << std::endl;
+					break;
+				}
+				}
+				break;
+			}
+			default:
+			{
+				std::cout << "Incorrect data entered" << std::endl;
+				break;
+			}
+			}
+			break;
+		}
+		case SEARCH:
+		{
+			menuWithCategoriesWhenSearching();
+			unsigned choosenCategory;
+			std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+			try
+			{
+				std::cin >> choosenCategory;
+				informationAboutSearch();
+				std::vector<Player> data = playersList.find(choosenCategory);
+				PlayersList foundData = PlayersList(data);
+				informationAboutWhatWasFound();
+				std::cout << foundData;
+			}
+			catch (std::istream::failure e)
+			{
+				std::cout << "Failed to enter data" << std::endl;
+				std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			break;
+		}
+		case EDIT_ITEM:
+		{
+			std::cout << playersList;
+			std::cout << "Enter the number of the element you want to edit" << std::endl;
+			unsigned elementToEdit;
+			std::cin.exceptions(std::istream::failbit | std::istream::badbit);
+			try
+			{
+				std::cin >> elementToEdit;
+				if (elementToEdit > playersList.getPlayersList().size()) {
+					break;
+				}
+				Player player = Player();
+				Player blankShot = Player();
+				informationAboutCorrectDataEntry();
+				std::cin >> player;
+				if (player == blankShot) {
+					break;
+				}
+				playersList.edit(elementToEdit, player);
+			}
+			catch (std::istream::failure e)
+			{
+				std::cout << "Failed to enter data" << std::endl;
+				std::cin.clear(); // на случай, если предыдущий ввод завершился с ошибкой
+				std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			}
+			break;
+		}
+		case CORE_TEAM_PARTICIPATED_IN_THE_LAST_MATCH_ONE_YELLOW_CARD:
+		{
+			std::vector<Player> data = playersList.areInTheMainTeamParticipatedInTheLastGameAndHaveOneYellowCard();
+			PlayersList foundData = PlayersList(data);
+			informationAboutWhatWasFound();
+			std::cout << foundData;
+			break;
+		}
+		case EXIT:
+		{
+			break;
+		}
+		default:
+		{	
+			std::cout << "Incorrect data entered" << std::endl;
+			break;
+		}
+		}
+
+		if (choice == EXIT) {
+			break;
+		}
+	}
+
+	return 0;
+}			
